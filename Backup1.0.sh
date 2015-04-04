057710feedf5232ea9096b72678c8da8a34d4804
Adam Shuman <shumanac@montreat.edu>
2015-04-04 11:21:36 -0400

    Update Backup1.0.sh

--- Backup1.0.sh
+++ Backup1.0.sh
@@ -133,7 +133,7 @@ fi
 if [[ $answermySQL = y ]] ; then
     kill `ps aux | grep mysqld`
     read -p "What would you like the password to be? " password
-    sudo echo UPDATE mysql.user SET Password='$password' WHERE User='root';'\n'FLUSH PRIVILEGES; >> /root/mysql-init
+    sudo echo UPDATE mysql.user SET Password='$password' WHERE User='root';'\n'FLUSH PRIVILEGES;'\n'quit; >> /root/mysql-init
     mysqld_safe --init-file=/root/mysql-init
     rm /home/root/mysql-init
     /etc/init.d/mysql restart
